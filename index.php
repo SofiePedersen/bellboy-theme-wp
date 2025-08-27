4b7ceab3dd4c765269d83c0171546830e8df91cd
SofiePedersen <sofie@gefiber.dk>
2025-08-27 12:18:58 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" href="<?php echo get_template_directory_uri()?>/style.css" rel="stylesheet">
+</head>
+<body>
+    <h1>Hej</h1>
+</body>
+</html>
