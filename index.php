3d5dab5a42fed8734a06c23ca8bc183f81eacd00
SofiePedersen <sofie@gefiber.dk>
2025-08-29 00:43:23 +0200

    Selecting Colors

--- index.php
+++ index.php
@@ -3,9 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SAP Portfolio</title>
     <link type="text/css" href="<?php echo get_template_directory_uri()?>/style.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Hej</h1>
+    <header class="header">
+
+    </header>
+    <main>
+        <div class="one">
+            <p>Color 1</p>
+        </div>
+        <div class="two">
+            <p>Color 2</p>
+        </div>
+        <div class="three">
+            <p>Color 3</p>
+        </div>
+        <div class="four">
+            <p>Color 4</p>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
 </body>
 </html>
