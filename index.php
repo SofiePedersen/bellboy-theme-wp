c02dc978ea57b9ddf48335280a1f40419e06a57c
SofiePedersen <sofie@gefiber.dk>
2025-08-29 15:57:48 +0200

    Changelog

--- index.php
+++ index.php
@@ -7,25 +7,5 @@
     <link type="text/css" href="<?php echo get_template_directory_uri()?>/style.css" rel="stylesheet">
 </head>
 <body>
-    <header class="header">
-
-    </header>
-    <main>
-        <div class="one">
-            <p>Color 1</p>
-        </div>
-        <div class="two">
-            <p>Color 2</p>
-        </div>
-        <div class="three">
-            <p>Color 3</p>
-        </div>
-        <div class="four">
-            <p>Color 4</p>
-        </div>
-    </main>
-    <footer>
-
-    </footer>
 </body>
 </html>
