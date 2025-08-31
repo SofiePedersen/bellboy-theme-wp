bc9f38410dce17dfb0c07b4ca7d751aff0764304
SofiePedersen <sofie@gefiber.dk>
2025-08-31 13:03:09 +0200

    CPT - projects

--- functions.php
+++ functions.php
@@ -1,6 +1,34 @@
 <?php
+// Her looper jeg mine menu-punkter ud
 register_nav_menus(array(
     'main_menu' => 'Main Menu',
     'footer_menu' => 'Footer Menu'
 ));
+
+// Forsøg på at oprette et custom faneblad (Cpt - Custom Post Type)
+function register_projects_post_type() {
+    // Opretter en CPT kaldt project
+    register_post_type('project', array(
+        // Opretter navnene på mine custom posts
+        'labels' => array(
+            'name' => __('Projects'),
+            'singular_name' => __('Project')
+        ),
+        // public true betyder besøgende kan se postet, 
+        // var den sat til false ville kun en admin kunne se den
+        'public' => true,
+        // has_archive gør at der automatisk oprettes et archive page når den er sat til true
+        'has_archive' => true,
+        // rewrite gør det muligt at styre hvordan URL'en skal se ud (navn)
+        'rewrite' => array('slug' => 'projects'),
+        // supports er hvilke features jeg gerne vil have tilgængeligt når jeg redigere min post
+        'supports' => array('title', 'editor', 'thumbnail', 'excerpt'),
+        //icon in admin sidebar
+        'menu_icon' => 'dashicons-portfolio'
+    ));
+}
+// WordPress hook - 
+// "Hooks are a system WordPress uses to let developers “hook into” or tap into the execution flow at specific points."
+add_action('init', 'register_projects_post_type');
 ?>
+
--- index.php
+++ index.php
@@ -1,2 +1,5 @@
 <?php get_header();?>
+
+<h1>This is my Index - Not the front-page</h1>
+
 <?php get_footer();?>
